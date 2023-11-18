4702a050585bfb09ab8411ab22505365b0048951
saiotest <elyermad@gmail.com>
2023-11-18 00:16:33 -0300

    making progress understanding cucumber playwright -- VERI DIFICUL xd

--- src/support/config.ts
+++ src/support/config.ts
@@ -1,5 +1,9 @@
 import { LaunchOptions } from '@playwright/test';
+import 'dotenv/config';
+
 const browserOptions: LaunchOptions = {
+  //* To change the variable headles, use: "export HEAD=true" to run test in terminal or "export HEAD=false" to do not.
+  headless: process.env.CI ? true : process.env.HEAD === 'true',
   slowMo: 0,
   args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
   firefoxUserPrefs: {
